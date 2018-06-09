ba997e1c8b59cde3280e68c8013ed5a61ff740ea
5820K <38088640+chuynh18@users.noreply.github.com>
2018-06-09 01:07:52 -0700

    made the project work with .env file for development MySQL

---
+++ config/config.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
+  }
+}
