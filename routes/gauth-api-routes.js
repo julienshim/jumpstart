5d61ff1c7c474f7bf0261e8f80d2d38b894cbcdc
Acer <38088640+chuynh18@users.noreply.github.com>
2018-06-11 13:58:53 -0700

    added comments to gauth-api-routes.js

--- routes/gauth-api-routes.js
+++ routes/gauth-api-routes.js
@@ -1,14 +1,17 @@
 var db = require("../models");
 
 module.exports = function(app) {
-
+    // bring in google's library as a dependency
     const {OAuth2Client} = require('google-auth-library');
 
+    // the route for our clients to authenticate
     app.post("/api/auth", function(req, res) {
+        // our application's CLIENT_ID provided by google
         const CLIENT_ID = "305106195609-ber51vabb990ftsd40aqr31gjthutaau.apps.googleusercontent.com";
 
         // console.log(req.body.idtoken);
 
+        // verify the ID Token being sent by the client
         const client = new OAuth2Client(CLIENT_ID);
         async function verify() {
             const ticket = await client.verifyIdToken({
@@ -28,24 +31,31 @@ module.exports = function(app) {
             console.log("userid is...");
             console.log(userid);
 
-            db.User.findOne({
-                where: {
-                  user_name: userName
-                }
-            }).then(function(dbUser) {
-                console.log(dbUser);
-                if (!dbUser) {
-                    db.User.create({
-                        user_name: userName,
-                        email: userMail
-                    }).then(function(dbUser) {
+            // perform appropriate db lookups after user authentication
+            if (userName) {
+                // see if the user already exists in our user table
+                db.User.findOne({
+                    where: {
+                      user_name: userName
+                    }
+                }).then(function(dbUser) {
+                    console.log(dbUser);
+                    // if the user doesn't yet exist, create them
+                    if (!dbUser) {
+                        db.User.create({
+                            user_name: userName,
+                            email: userMail
+                        }).then(function(dbUser) {
+                            res.json(dbUser);
+                        });
+                    }
+                    // otherwise, if they exist, return their data from the table
+                    // the data we're most interested in will be under dbUser.dataValues
+                    else {
                         res.json(dbUser);
-                    });
-                }
-                else {
-                    res.json(dbUser);
-                }
-            });
+                    }
+                });
+            }
         }
         verify().catch(console.error);
     });
