c55c3ae1fa48211634d5c41e1b86757e6337f7df
Chris (from laptop/git desktop app) <38088640+chuynh18@users.noreply.github.com>
2018-06-14 16:29:34 -0700

    reload leads page after update is written to the db

--- public/javascript/leadsUpdate.js
+++ public/javascript/leadsUpdate.js
@@ -11,48 +11,49 @@ var onLeadClick = function(element) {
         var response = JSON.parse(xhr.response);
         if ("company" in response) {
 
-                        // populate the modal that shows the user's submission, then make it visible
-                        document.getElementById("companyView").value = response.company;
-                        document.getElementById("positionView").value = response.position;
-                        document.getElementById("jobUrlView").value = response.leadLink;
-                        document.getElementById("applicationDateView").value = response.dateApplied;
-                        document.getElementById("jobDocumentsView").value = response.documents;
-                        document.getElementById("jobNotesView").textContent = response.notes;
-                        $("#jobView").modal('show');
+            // populate the modal that shows the user's submission, then make it visible
+            document.getElementById("companyView").value = response.company;
+            document.getElementById("positionView").value = response.position;
+            document.getElementById("jobUrlView").value = response.leadLink;
+            document.getElementById("applicationDateView").value = response.dateApplied;
+            document.getElementById("jobDocumentsView").value = response.documents;
+            document.getElementById("jobNotesView").textContent = response.notes;
+            $("#jobView").modal('show');
         }
    }
 
-        xhr.open("GET", `/api/leads/${userId}/${leadId}`);
-        xhr.send();
+    xhr.open("GET", `/api/leads/${userId}/${leadId}`);
+    xhr.send();
 
 }
 
 var onLeadUpdate = function() {
 
-        if (!userId) {
-            document.getElementById("signInAlert").textContent = "Please sign in first!";
-        }
-        else {
+    if (!userId) {
+        document.getElementById("signInAlert").textContent = "Please sign in first!";
+    }
+    else {
 
-                var company = document.getElementById("companyView").value.trim();
-                var position = document.getElementById("positionView").value.trim();
-                var jobUrl = document.getElementById("jobUrlView").value.trim();
-                var applicationDate = document.getElementById("applicationDateView").value.trim();
-                var documentUrl = document.getElementById("jobDocumentsView").value.trim();
-                var jobNotes = document.getElementById("jobNotesView").value.trim();
+        var company = document.getElementById("companyView").value.trim();
+        var position = document.getElementById("positionView").value.trim();
+        var jobUrl = document.getElementById("jobUrlView").value.trim();
+        var applicationDate = document.getElementById("applicationDateView").value.trim();
+        var documentUrl = document.getElementById("jobDocumentsView").value.trim();
+        var jobNotes = document.getElementById("jobNotesView").value.trim();
+
+
+        var formData = `id=${leadId}&company=${company}&position=${position}&leadLink=${jobUrl}&dateApplied=${applicationDate}&documents=${documentUrl}&notes=${jobNotes}&UserId=${userId}`;
         
-        
-                var formData = `id=${leadId}&company=${company}&position=${position}&leadLink=${jobUrl}&dateApplied=${applicationDate}&documents=${documentUrl}&notes=${jobNotes}&UserId=${userId}`;
-                
-                var xhr = new XMLHttpRequest();
+        var xhr = new XMLHttpRequest();
 
-                xhr.open("PUT", "/api/leads");
-                xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                xhr.send(formData);
+        xhr.open("PUT", "/api/leads");
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send(formData);
 
+        xhr.onload = function() {
+            window.location.reload();
         }
-
-
+    }
 }   
 
 
