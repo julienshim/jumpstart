867efe671cc8f5c9b7653a85b00b26c3fb485f21
Chris (from laptop/git desktop app) <38088640+chuynh18@users.noreply.github.com>
2018-06-14 16:21:01 -0700

    added some instructional text to the homepage after user logs in

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,5 +1,5 @@
 "use strict";
 
 givenNameObj.registerListener(function(val) {
-    document.getElementById("loginMsg").textContent = `Welcome to JumpStart, ${val}!`;
+    document.getElementById("loginMsg").textContent = `Welcome to JumpStart, ${val}!  You can access the many features of JumpStart via the navigation bar to the left.`;
 });
